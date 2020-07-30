1e3f89eb0b73f2a6b75a382049c9514491b6c7fb
Michael Campbell <michael.campbell14@gmail.com>
2020-07-30 14:50:46 -0400

    finished day 1, lessons 1-5

--- React-For-Beginners-Starter-Files-master/catch-of-the-day/src/index.js
+++ React-For-Beginners-Starter-Files-master/catch-of-the-day/src/index.js
@@ -1,2 +1,6 @@
-// let's go!
-alert("hey");
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker.js';
+import "./css/style.css";
+
+render(<StorePicker />, document.querySelector('#main'));
